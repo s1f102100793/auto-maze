705d5ea323218b6490e39492eb0b44680f179aa6
s1f102100793 <s1f102100793@iniad.org>
2023-07-18 20:58:56 +0900

    迷路のconsole上では完成

---
+++ src/hooks/useBoard.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export const useBoard = () => {
+  const [maze, setMaze] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+
+  const directions: number[][] = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+  ];
+  const newMaze: number[][] = JSON.parse(JSON.stringify(maze));
+
+  return { maze, setMaze, directions, newMaze};
+};
---
+++ src/hooks/useGame.tsx
@@ -0,0 +1,37 @@
+import { useBoard } from './useBoard';
+
+export const useGame = () => {
+  const { maze, setMaze, directions, newMaze } = useBoard();
+
+  const iterateBoard = (callback: (y: number, x: number) => void) => {
+    for (let y = 0; y < 9; y++) {
+      for (let x = 0; x < 9; x++) {
+        callback(y, x);
+      }
+    }
+  };
+
+  const Itteme = (y: number, x: number) => {
+    if (y % 2 === 1 && x % 2 === 1) {
+      const falldirection = directions[Math.floor(Math.random() * directions.length)];
+      if (maze[y + falldirection[0]] !== undefined && maze[x + falldirection[1]] !== undefined) {
+        newMaze[y + falldirection[0]][x + falldirection[1]] = 2;
+        newMaze[y][x] = 1;
+      }
+    }
+  };
+
+  const allOne = (y: number, x: number) => {
+    if (newMaze[y][x] === 2) {
+      newMaze[y][x] = 1;
+      setMaze(newMaze);
+    }
+  };
+
+  const onClick = () => {
+    iterateBoard(Itteme);
+    iterateBoard(allOne);
+  };
+  
+  return { maze, setMaze, onClick };
+};
