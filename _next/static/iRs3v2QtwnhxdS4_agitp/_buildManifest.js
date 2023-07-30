12e04b9ab74894245115e535c47b716a769ce56d
s1f102100793 <s1f102100793@users.noreply.github.com>
2023-07-30 06:14:07 +0000

    deploy: 606c1a4540586ffc98908b21e0055e7d87c35af0

--- _next/static/iRs3v2QtwnhxdS4_agitp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/906649bf0b4a0cfd.css","static/chunks/pages/index-01981f97e441adb6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
